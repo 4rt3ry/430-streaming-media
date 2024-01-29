dcdaa7aff35918dbe8b3977ccf4bf42375b3dfd3
Arthur Powers (RIT Student) <aap5744@rit.edu>
2024-01-29 14:51:24 -0500

    started working on hw

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const htmlHandler = require("./htmlResponses.js");
+const mediaHandler = require("./mediaResponses.js");
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const requestHandler = {
+    "/": htmlHandler.getIndex,
+    "/asdf": htmlHandler.getIndex,
+    default: htmlHandler.getIndex
+}
+
+const onRequest = (request, response) => {
+    // console.log(request);
+
+    const parsedUrl = new URL(request.url, `http://localhost:${port}`);
+    const acceptedTypes = request.headers.accept.split(",");
+    const handler = requestHandler[parsedUrl];
+
+    if (handler) handler(request, response);
+    else requestHandler.default(request, response);
+}
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1:${port}`);
+})
